afc2e5db1442573ab32b1694497f2e6df289a1fe
Washim Akram <90306771+AlienWashim@users.noreply.github.com>
2024-11-29 18:16:48 +0600

    Restructured to understand each concept individually

--- Delegates_Events_Lambdas/Program.cs
+++ Delegates_Events_Lambdas/Program.cs
@@ -27,26 +27,6 @@ class Delegate
         // When we invoke print2 with a string, it internally calls the PrintMessageConsole method.
         // This is equivalent to calling PrintMessageConsole("Again Nice Learning").
         print2("Again Nice Learning");
-
-
-
-
-
-        // Defining a lambda expression and assigning it to a variable
-        // 'Func<int, int, int>' specifies a delegate type that takes two integers (int, int) and returns an integer (int).
-        // The lambda expression '(a, b) => a + b' defines the functionality: adding the two integers together.
-        Func<int, int, int> add = (a, b) => a + b;
-
-        // Calling the lambda expression by passing two integers (3 and 4) as arguments.
-        // The lambda expression will add these numbers together and return the result (7).
-        // The result is then passed to Console.WriteLine to display the output.
-        Console.WriteLine(add(3, 4));  // Outputs: 7
-
-        // Subscribe to the event with a lambda expression that will be called when the event is triggered
-        OnNotify += (msg) => Console.WriteLine($"Event Triggered: {msg}");
-
-        // Trigger the event, which will invoke all the subscribed handlers
-        TriggerEvent();
     }
 
     // This is our custom method that matches the delegate signature.
@@ -55,21 +35,6 @@ class Delegate
     public static void PrintMessageConsole(string message)
     {
         Console.WriteLine(message); // Outputs the message to the console.
-    }
-
-
-    // Declare a delegate that matches the signature of event handlers
-    // This delegate takes a string parameter and returns void.
-    public delegate void NotifyEventHandler(string message);
-
-    // Declare the event based on the delegate
-    public static event NotifyEventHandler OnNotify;
-
-    // Define a method to trigger the event
-    public static void TriggerEvent()
-    {
-        // Safely invoke the event if there are subscribers
-        OnNotify?.Invoke("User logged in!");
     }
 }
 
